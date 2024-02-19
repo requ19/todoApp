8876b673c96ab810475762b4824c6a562b3fb76b
requ19 <azizmailcom1910@gmail.com>
2024-02-19 09:11:21 +0600

    new button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 const Button = ({children, onClick, value = '', activeBtn = false}) => {
   return (
-    <button className='btn' type='button' onClick={onClick} value={value} style={{background: activeBtn ? 'rgb(222, 222, 0)' : '#fff'}}>{children}</button>
+    <button className='btn' type='button' onClick={onClick} value={value} style={{background: activeBtn ? 'rgb(152, 0, 222)' : '#fff'}}>{children}</button>
   )
 }
 
